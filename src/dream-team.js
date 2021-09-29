0d673b498c6b5c02a7d72330a019a80c94099295
ilya7am <88287882+ilya7am@users.noreply.github.com>
2021-09-29 14:47:15 +0400

    feat: code for hanoi-tower

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,14 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  if(!Array.isArray(members)) { return false };
+  let nameArr = [];
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === "string" && members[i].trim()) 
+    nameArr.push(members[i].trim()[0]);
+  }
+
+  return nameArr.join("").toUpperCase().split("").sort()
+
 }
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,7 +14,9 @@ import { NotImplementedError } from '../extensions/index.js';
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-export default function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function calculateHanoi(disksNumber, turnsSpeed) {
+  let turnsAmount = 2 ** disksNumber - 1;
+  let everySec = turnsSpeed / 3600;
+  let secondsAmount = Math.floor(turnsAmount / everySec);
+  return {turns: turnsAmount, seconds: secondsAmount}
 }
